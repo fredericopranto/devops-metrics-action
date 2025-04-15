75fd478a4fc8e78b25266c23d95143c96ab6a30e
Frederico Pranto <fredericopranto@gmail.com>
2025-04-15 19:12:57 -0300

    ajuste nos testes de CFR

--- __tests__/ChangeFailureRate.test.ts
+++ __tests__/ChangeFailureRate.test.ts
@@ -83,8 +83,8 @@ describe('ChangeFailureRate should', () => {
 
     const releases = [
       {
-        url: 'https://api.github.com/repos/fredericopranto/mock/releases/101411508',
-        published_at: '2025-01-01T10:00:00Z'
+        published_at: '2025-01-01T10:00:00Z',
+        url: 'https://api.github.com/repos/fredericopranto/mock/releases/id',
       }
     ] as Release[]
 
@@ -109,8 +109,8 @@ describe('ChangeFailureRate should', () => {
 
     const releases = [
       {
-        url: 'https://api.github.com/repos/fredericopranto/mock/releases/101411508',
-        published_at: '2025-01-02T10:00:00Z'
+        published_at: '2025-01-02T10:00:00Z',
+        url: 'https://api.github.com/repos/fredericopranto/mock/releases/id',        
       }
     ] as Release[]
 
@@ -124,20 +124,20 @@ describe('ChangeFailureRate should', () => {
   it('calculate 50% failures on 1 issues on 2 releases', () => {
     const bugs = [
       {
-        created_at: '2023-04-30T17:50:53Z',
-        repository_url: 'somepath/repository',
-        labels: [{name: 'bug'}]
+        created_at: '2025-01-02T10:00:00Z',
+        labels: [{name: 'bug'}],
+        repository_url: 'https://api.github.com/repos/fredericopranto/mock'
       }
     ] as Issue[]
 
     const releases = [
       {
-        published_at: '2023-04-30T16:50:53Z',
-        url: 'path/with/repository/in/it'
+        published_at: '2025-01-01T10:00:00Z',
+        url: 'https://api.github.com/repos/fredericopranto/mock/releases/id',  
       },
       {
-        published_at: '2023-04-29T16:50:53Z',
-        url: 'path/with/repository/in/it'
+        published_at: '2025-01-03T10:00:00Z',
+        url: 'https://api.github.com/repos/fredericopranto/mock/releases/id',  
       }
     ] as Release[]
 
@@ -154,20 +154,20 @@ describe('ChangeFailureRate should', () => {
   it('calculate 0% failures on 1 issues that is not a bug on 2 releases', () => {
     const bugs = [
       {
-        created_at: '2023-04-30T17:50:53Z',
-        repository_url: 'somepath/repository',
-        labels: [{name: 'feature'}]
+        created_at: '2025-01-01T10:00:00Z',
+        labels: [{name: 'feature'}],
+        repository_url: 'https://api.github.com/repos/fredericopranto/mock'
       }
     ] as Issue[]
 
     const releases = [
       {
-        published_at: '2023-04-30T16:50:53Z',
-        url: 'path/with/repository/in/it'
+        published_at: '2025-01-02T10:00:00Z',
+        url: 'https://api.github.com/repos/fredericopranto/mock/releases/id',  
       },
       {
-        published_at: '2023-04-29T16:50:53Z',
-        url: 'path/with/repository/in/it'
+        published_at: '2025-01-02T10:00:00Z',
+        url: 'https://api.github.com/repos/fredericopranto/mock/releases/id',  
       }
     ] as Release[]
 
@@ -184,25 +184,25 @@ describe('ChangeFailureRate should', () => {
   it('calculate 50% failures on 2 issues after latest release', () => {
     const bugs = [
       {
-        created_at: '2023-04-30T17:50:53Z',
-        repository_url: 'somepath/repository',
-        labels: [{name: 'bug'}]
+        created_at: '2025-01-02T10:00:00Z',
+        labels: [{name: 'bug'}],
+        repository_url: 'https://api.github.com/repos/fredericopranto/mock'
       },
       {
-        created_at: '2023-04-30T17:50:53Z',
-        repository_url: 'somepath/repository',
-        labels: [{name: 'bug'}]
+        created_at: '2025-01-02T10:00:00Z',
+        labels: [{name: 'bug'}],
+        repository_url: 'https://api.github.com/repos/fredericopranto/mock'
       }
     ] as Issue[]
 
     const releases = [
       {
-        published_at: '2023-04-30T16:50:53Z',
-        url: 'path/with/repository/in/it'
+        published_at: '2025-01-01T10:00:00Z',
+        url: 'https://api.github.com/repos/fredericopranto/mock/releases/id',  
       },
       {
-        published_at: '2023-04-29T16:50:53Z',
-        url: 'path/with/repository/in/it'
+        published_at: '2025-01-03T10:00:00Z',
+        url: 'https://api.github.com/repos/fredericopranto/mock/releases/id',  
       }
     ] as Release[]
 
@@ -216,28 +216,28 @@ describe('ChangeFailureRate should', () => {
     expect(value).toBe(50)
   })
 
-  it('calculate 100% failures on 2 issues after no releases this month', () => {
+  it('calculate 100% failures on 2 issues after no releases', () => {
     const bugs = [
       {
-        created_at: '2023-04-29T17:50:53Z',
-        repository_url: 'somepath/repository',
-        labels: [{name: 'bug'}]
+        created_at: '2025-02-01T10:00:00Z',
+        labels: [{name: 'bug'}],
+        repository_url: 'https://api.github.com/repos/fredericopranto/mock'
       },
       {
-        created_at: '2023-04-31T17:50:53Z',
-        repository_url: 'somepath/repository',
-        labels: [{name: 'bug'}]
+        created_at: '2025-02-02T10:00:00Z',
+        labels: [{name: 'bug'}],
+        repository_url: 'https://api.github.com/repos/fredericopranto/mock'
       }
     ] as Issue[]
 
     const releases = [
       {
-        published_at: '2023-04-30T16:50:53Z',
-        url: 'path/with/repository/in/it'
+        published_at: '2025-01-01T10:00:00Z',
+        url: 'https://api.github.com/repos/fredericopranto/mock/releases/id',  
       },
       {
-        published_at: '2023-04-29T16:50:53Z',
-        url: 'path/with/repository/in/it'
+        published_at: '2025-01-02T10:00:00Z',
+        url: 'https://api.github.com/repos/fredericopranto/mock/releases/id',  
       }
     ] as Release[]
 
@@ -251,33 +251,33 @@ describe('ChangeFailureRate should', () => {
     expect(value).toBe(100)
   })
 
-  it('calculate 100% failures on 2 issues after latest release and one before', () => {
+  it('calculate 100% failures on 2 bug issues and 1 feature issue after latest release and one before', () => {
     const bugs = [
       {
-        created_at: '2023-04-29T17:50:53Z',
-        repository_url: 'somepath/repository',
-        labels: [{name: 'bug'}]
+        created_at: '2025-02-01T10:00:00Z',
+        labels: [{name: 'bug'}],
+        repository_url: 'https://api.github.com/repos/fredericopranto/mock'
       },
       {
-        created_at: '2023-04-30T17:50:53Z',
-        repository_url: 'somepath/repository',
-        labels: [{name: 'bug'}]
+        created_at: '2025-02-02T10:00:00Z',
+        labels: [{name: 'bug'}],
+        repository_url: 'https://api.github.com/repos/fredericopranto/mock'
       },
       {
-        created_at: '2023-04-30T17:50:53Z',
-        repository_url: 'somepath/repository',
-        labels: [{name: 'bug'}]
+        created_at: '2025-02-03T10:00:00Z',
+        labels: [{name: 'feature'}],
+        repository_url: 'https://api.github.com/repos/fredericopranto/mock'
       }
     ] as Issue[]
 
     const releases = [
       {
-        published_at: '2023-04-30T16:50:53Z',
-        url: 'path/with/repository/in/it'
+        published_at: '2025-01-02T10:00:00Z',
+        url: 'https://api.github.com/repos/fredericopranto/mock/releases/id',  
       },
       {
-        published_at: '2023-04-29T16:50:53Z',
-        url: 'path/with/repository/in/it'
+        published_at: '2025-01-03T10:00:00Z',
+        url: 'https://api.github.com/repos/fredericopranto/mock/releases/id',  
       }
     ] as Release[]
 
@@ -294,34 +294,34 @@ describe('ChangeFailureRate should', () => {
   it('calculate 33% failures on 3 issues after first release and none after second of 3 releases', () => {
     const bugs = [
       {
-        created_at: '2023-04-29T17:50:53Z',
-        repository_url: 'somepath/repository',
-        labels: [{name: 'bug'}]
+        created_at: '2025-02-02T10:00:00Z',
+        labels: [{name: 'bug'}],
+        repository_url: 'https://api.github.com/repos/fredericopranto/mock'
       },
       {
-        created_at: '2023-04-30T17:50:53Z',
-        repository_url: 'somepath/repository',
-        labels: [{name: 'bug'}]
+        created_at: '2025-02-02T10:00:00Z',
+        labels: [{name: 'bug'}],
+        repository_url: 'https://api.github.com/repos/fredericopranto/mock'
       },
       {
-        created_at: '2023-04-30T17:50:53Z',
-        repository_url: 'somepath/repository',
-        labels: [{name: 'bug'}]
+        created_at: '2025-02-02T10:00:00Z',
+        labels: [{name: 'bug'}],
+        repository_url: 'https://api.github.com/repos/fredericopranto/mock'
       }
     ] as Issue[]
 
     const releases = [
       {
-        published_at: '2023-04-28T16:50:53Z',
-        url: 'path/with/repository/in/it'
+        published_at: '2025-01-03T10:00:00Z',
+        url: 'https://api.github.com/repos/fredericopranto/mock/releases/id',  
       },
       {
-        published_at: '2023-04-30T19:50:53Z',
-        url: 'path/with/repository/in/it'
+        published_at: '2025-03-03T10:00:00Z',
+        url: 'https://api.github.com/repos/fredericopranto/mock/releases/id',  
       },
       {
-        published_at: '2023-05-02T16:50:53Z',
-        url: 'path/with/repository/in/it'
+        published_at: '2025-03-03T10:00:00Z',
+        url: 'https://api.github.com/repos/fredericopranto/mock/releases/id',  
       }
     ] as Release[]
 
@@ -335,95 +335,4 @@ describe('ChangeFailureRate should', () => {
     expect(value).toBe(33)
   })
 
-  it('calculate 50% failures on 3 issues on two repos on two of 4 releases', () => {
-    const bugs = [
-      {
-        created_at: '2023-04-29T17:50:53Z', // bug on 28, repository
-        repository_url: 'somepath/repository',
-        labels: [{name: 'bug'}]
-      },
-      {
-        created_at: '2023-04-30T17:50:53Z', // bug on 20, other-repo
-        repository_url: 'somepath/other-repo',
-        labels: [{name: 'bug'}]
-      },
-      {
-        created_at: '2023-04-30T17:50:53Z', // bug on 28, repository
-        repository_url: 'somepath/repository',
-        labels: [{name: 'bug'}]
-      }
-    ] as Issue[]
-
-    const releases = [
-      {
-        published_at: '2023-04-28T16:50:53Z',
-        url: 'path/with/repository/in/it'
-      },
-      {
-        published_at: '2023-04-28T16:50:53Z',
-        url: 'path/with/other-repo/in/it'
-      },
-      {
-        published_at: '2023-04-30T19:50:53Z',
-        url: 'path/with/repository/in/it'
-      },
-      {
-        published_at: '2023-05-02T16:50:53Z',
-        url: 'path/with/repository/in/it'
-      }
-    ] as Release[]
-
-    const cfr = new ChangeFailureRate(
-      bugs,
-      releases,
-    )
-
-    const value = cfr.Cfr()
-
-    expect(value).toBe(50)
-  })
-
-  it('calculate 50% failures on 3 issues after first release no older than a month', () => {
-    const bugs = [
-      {
-        created_at: '2023-04-29T17:50:53Z',
-        repository_url: 'somepath/repository',
-        labels: [{name: 'bug'}]
-      },
-      {
-        created_at: '2023-04-30T17:50:53Z',
-        repository_url: 'somepath/repository',
-        labels: [{name: 'bug'}]
-      },
-      {
-        created_at: '2023-04-30T17:50:53Z',
-        repository_url: 'somepath/repository',
-        labels: [{name: 'bug'}]
-      }
-    ] as Issue[]
-
-    const releases = [
-      {
-        published_at: '2023-04-28T16:50:53Z',
-        url: 'path/with/repository/in/it'
-      },
-      {
-        published_at: '2023-04-30T19:50:53Z',
-        url: 'path/with/repository/in/it'
-      },
-      {
-        published_at: '2023-04-02T16:50:53Z',
-        url: 'path/with/repository/in/it'
-      }
-    ] as Release[]
-
-    const cfr = new ChangeFailureRate(
-      bugs,
-      releases,
-    )
-
-    const value = cfr.Cfr()
-
-    expect(value).toBe(50)
-  })
 })
