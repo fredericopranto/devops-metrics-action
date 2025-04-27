b2834b3547e9bdd660d74af1832b79eb0c7fbe87
Frederico Pranto <fredericopranto@gmail.com>
2025-04-27 03:08:26 -0300

    refac df

--- __tests__/dora/DeployFrequency.test.ts
+++ __tests__/dora/DeployFrequency.test.ts
@@ -2,16 +2,18 @@ import {DeployFrequency} from '../../src/DeployFrequency'
 import {Release} from '../../src/types/Release'
 import { addTime, TimeUnit } from '../../src/utils/DateUtils';
 
-describe('Deploy frequency should', () => {
-  const dayOne = new Date(2025, 0, 1)
-  it('calculate total days between two dates', () => {
+describe('Deploy frequency ', () => {
+  const dayOne = new Date(2025, 0, 1);
+
+  it('should calculate total days between two dates', () => {
     const releases = [] as Release[];
     
     expect(new DeployFrequency(releases, dayOne, dayOne).getTotalDays()).toBe(1);
     expect(new DeployFrequency(releases, dayOne, addTime(dayOne, 1, TimeUnit.Day)).getTotalDays()).toBe(2);
     expect(new DeployFrequency(releases, dayOne, addTime(dayOne, 2, TimeUnit.Day)).getTotalDays()).toBe(3);
-  })
-  it('calculate df for 2 releases away from interval', () => {
+  });
+
+  it('should return null for 2 releases outside the interval', () => {
     const releases = [
       { published_at: '2024-12-31T10:00:00Z' },
       { published_at: '2025-01-09T10:00:00Z' }
@@ -19,8 +21,9 @@ describe('Deploy frequency should', () => {
     
     const value = new DeployFrequency(releases, dayOne, new Date('2025-01-08')).rate();
     expect(value).toBe(null);
-  })
-  it('calculate df for 2 releases far away from interval', () => {
+  });
+
+  it('should return null for 2 releases far outside the interval', () => {
     const releases = [
       { published_at: '1800-01-01T10:00:00Z' },
       { published_at: '3100-01-01T10:00:00Z' } 
@@ -28,8 +31,9 @@ describe('Deploy frequency should', () => {
     
     const value = new DeployFrequency(releases, dayOne, new Date('2025-01-08')).rate();
     expect(value).toBe(null);
-  })
-  it('calculate df for 1 release with dates in wrong order', () => {
+  });
+
+  it('should throw an error for 1 release with dates in the wrong order', () => {
     const releases = [
       { published_at: '2024-12-31T10:00:00Z' },
     ] as Release[];
@@ -37,8 +41,9 @@ describe('Deploy frequency should', () => {
     expect(() => {
       new DeployFrequency(releases, new Date('2025-01-02'), new Date('2025-01-01')).rate();
     }).toThrow('Start date must be before end date');
-  })
-  it('calculate df for 1 release with invalid date', () => {
+  });
+
+  it('should throw an error for 1 release with an invalid date', () => {
     const releases = [
       { published_at: '' }
     ] as Release[];
@@ -46,14 +51,16 @@ describe('Deploy frequency should', () => {
     expect(() => {
       new DeployFrequency(releases, dayOne, addTime(dayOne, 1, TimeUnit.Day)).rate();
     }).toThrow('Invalid release date format');
-  })
-  it('calculate df for 0 release in 1 day', () => {
+  });
+
+  it('should return null for 0 releases in 1 day', () => {
     const releases = [] as Release[];
 
     const value = new DeployFrequency(releases, dayOne, dayOne).rate();
-    expect(value).toBe(null)
-  })
-  it('calculate df for 1 release (with created_at) in 1 day', () => {
+    expect(value).toBe(null);
+  });
+
+  it('should calculate deploy frequency for 1 release (with created_at) in 1 day', () => {
     const releases = [
       {
         created_at: '2025-01-01T10:00:00Z'
@@ -61,9 +68,10 @@ describe('Deploy frequency should', () => {
     ] as Release[];
 
     const value = new DeployFrequency(releases, dayOne, dayOne).rate();
-    expect(value).toBe(1)
-  })
-  it('calculate df for 1 release in 1 day', () => {
+    expect(value).toBe(1);
+  });
+
+  it('should calculate deploy frequency for 1 release in 1 day', () => {
     const releases = [
       {
         published_at: '2025-01-01T10:00:00Z'
@@ -71,49 +79,54 @@ describe('Deploy frequency should', () => {
     ] as Release[];
 
     const value = new DeployFrequency(releases, dayOne, dayOne).rate();
-    expect(value).toBe(1)
-  })
-  it('calculate df for 1 release in 2 days', () => {
+    expect(value).toBe(1);
+  });
+
+  it('should calculate deploy frequency for 1 release in 2 days', () => {
     const releases = [
       {
         published_at: '2025-01-01T10:00:00Z'
       }
-    ] as Release[]
+    ] as Release[];
 
     const value = new DeployFrequency(releases, dayOne, addTime(dayOne, 1, TimeUnit.Day)).rate();
-    expect(value).toBe(2)
-  })
-  it('calculate df for 1 release in 3 days', () => {
+    expect(value).toBe(2);
+  });
+
+  it('should calculate deploy frequency for 1 release in 3 days', () => {
     const releases = [
       {
         published_at: '2025-01-01T10:00:00Z'
       }
-    ] as Release[]
+    ] as Release[];
 
     const value = new DeployFrequency(releases, dayOne, addTime(dayOne, 2, TimeUnit.Day)).rate();
-    expect(value).toBe(3)
-  })
-  it('calculate df for 1 release in 1 week', () => {
+    expect(value).toBe(3);
+  });
+
+  it('should calculate deploy frequency for 1 release in 1 week', () => {
     const releases = [
       {
         published_at: '2025-01-01T10:00:00Z'
       }
-    ] as Release[]
+    ] as Release[];
 
     const value = new DeployFrequency(releases, dayOne, addTime(dayOne, 1, TimeUnit.Week)).rate();
-    expect(value).toBe(8)
-  })
-  it('calculate df for 1 release in 1 week', () => {
+    expect(value).toBe(8);
+  });
+
+  it('should calculate deploy frequency for 1 release in 1 month', () => {
     const releases = [
       {
         published_at: '2025-01-01T10:00:00Z'
       }
-    ] as Release[]
+    ] as Release[];
 
     const value = new DeployFrequency(releases, dayOne, addTime(dayOne, 1, TimeUnit.Month)).rate();
-    expect(value).toBe(32)
-  })
-  it('calculate df for 2 release (same datetime) in 1 day', () => {
+    expect(value).toBe(32);
+  });
+
+  it('should calculate deploy frequency for 2 releases (same datetime) in 1 day', () => {
     const releases = [
       {
         published_at: '2025-01-01T10:00:00Z'
@@ -121,12 +134,13 @@ describe('Deploy frequency should', () => {
       {
         published_at: '2025-01-01T10:00:00Z'
       }
-    ] as Release[]
+    ] as Release[];
 
     const value = new DeployFrequency(releases, dayOne, dayOne).rate();
-    expect(value).toBe(0.5)
-  })
-  it('calculate df for 2 release (same date) in 1 day', () => {
+    expect(value).toBe(0.5);
+  });
+
+  it('should calculate deploy frequency for 2 releases (same date) in 1 day', () => {
     const releases = [
       {
         published_at: '2025-01-01T10:00:00Z'
@@ -134,12 +148,13 @@ describe('Deploy frequency should', () => {
       {
         published_at: '2025-01-01T11:00:00Z'
       }
-    ] as Release[]
+    ] as Release[];
 
     const value = new DeployFrequency(releases, dayOne, dayOne).rate();
-    expect(value).toBe(0.5)
-  })
-  it('calculate df for 2 release in 1 day', () => {
+    expect(value).toBe(0.5);
+  });
+
+  it('should calculate deploy frequency for 2 releases in 1 day', () => {
     const releases = [
       {
         published_at: '2025-01-01T00:00:01Z'
@@ -147,12 +162,13 @@ describe('Deploy frequency should', () => {
       {
         published_at: '2025-01-01T23:59:59Z'
       }
-    ] as Release[]
+    ] as Release[];
 
     const value = new DeployFrequency(releases, dayOne, dayOne).rate();
-    expect(value).toBe(0.5)
-  })
-  it('calculate df for 2 release in 2 days', () => {
+    expect(value).toBe(0.5);
+  });
+
+  it('should calculate deploy frequency for 2 releases in 2 days', () => {
     const releases = [
       {
         published_at: '2025-01-01T10:00:00Z'
@@ -160,12 +176,13 @@ describe('Deploy frequency should', () => {
       {
         published_at: '2025-01-02T10:00:00Z'
       }
-    ] as Release[]
+    ] as Release[];
 
     const value = new DeployFrequency(releases, dayOne, addTime(dayOne, 1, TimeUnit.Day)).rate();
-    expect(value).toBe(1)
-  })
-  it('calculate df for 2 release (non-chronological) in 2 days', () => {
+    expect(value).toBe(1);
+  });
+
+  it('should calculate deploy frequency for 2 releases (non-chronological) in 2 days', () => {
     const releases = [
       {
         published_at: '2025-01-02T10:00:00Z'
@@ -173,49 +190,49 @@ describe('Deploy frequency should', () => {
       {
         published_at: '2025-01-01T10:00:00Z'
       }
-    ] as Release[]
+    ] as Release[];
 
     const value = new DeployFrequency(releases, dayOne, addTime(dayOne, 1, TimeUnit.Day)).rate();
-    expect(value).toBe(1)
-  })
-  it('calculate df for 100 releases in 100 days', () => {
+    expect(value).toBe(1);
+  });
+
+  it('should calculate deploy frequency for 100 releases in 100 days', () => {
     const days = 100;
     const releases = Array.from({ length: days }, (_, i) => ({
       published_at: new Date(2025, 0, 1 + i).toISOString() 
     })) as Release[];
   
-    const value = new DeployFrequency(releases, dayOne, addTime(dayOne, days-1, TimeUnit.Day)).rate();
-  
+    const value = new DeployFrequency(releases, dayOne, addTime(dayOne, days - 1, TimeUnit.Day)).rate();
     expect(value).toBe(1);
   });
-  it('calculate df for 1.000 releases in 1.000 days', () => {
+
+  it('should calculate deploy frequency for 1,000 releases in 1,000 days', () => {
     const days = 1000;
     const releases = Array.from({ length: days }, (_, i) => ({
       published_at: new Date(2025, 0, 1 + i).toISOString() 
     })) as Release[];
   
-    const value = new DeployFrequency(releases, dayOne, addTime(dayOne, days-1, TimeUnit.Day)).rate();
-  
+    const value = new DeployFrequency(releases, dayOne, addTime(dayOne, days - 1, TimeUnit.Day)).rate();
     expect(value).toBe(1);
   });
-  it('calculate df for 10.000 releases in 10.000 days', () => {
+
+  it('should calculate deploy frequency for 10,000 releases in 10,000 days', () => {
     const days = 10000;
     const releases = Array.from({ length: days }, (_, i) => ({
       published_at: new Date(2025, 0, 1 + i).toISOString() 
     })) as Release[];
   
-    const value = new DeployFrequency(releases, dayOne, addTime(dayOne, days-1, TimeUnit.Day)).rate();
-  
+    const value = new DeployFrequency(releases, dayOne, addTime(dayOne, days - 1, TimeUnit.Day)).rate();
     expect(value).toBe(1);
   });
-  it('calculate df for 100.000 releases in 100.000 days', () => {
+
+  it('should calculate deploy frequency for 100,000 releases in 100,000 days', () => {
     const days = 100000;
     const releases = Array.from({ length: days }, (_, i) => ({
       published_at: new Date(2025, 0, 1 + i).toISOString() 
     })) as Release[];
   
-    const value = new DeployFrequency(releases, dayOne, addTime(dayOne, days-1, TimeUnit.Day)).rate();
-  
+    const value = new DeployFrequency(releases, dayOne, addTime(dayOne, days - 1, TimeUnit.Day)).rate();
     expect(value).toBe(1);
   });
-})
+});
